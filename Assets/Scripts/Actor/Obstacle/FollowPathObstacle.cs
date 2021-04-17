ca6631f07ced777421e830111e14e19fe8f6122e
Fabpk90 <fabrizio1996@live.fr>
2021-04-17 11:54:24 +0200

    Added obstacles

---
+++ Assets/Scripts/Actor/Obstacle/FollowPathObstacle.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPathObstacle : MonoBehaviour
+{
+    public GameObject[] path;
+    public int startingIndex;
+
+    public float movementSpeed;
+
+    private Vector3 _destination;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _destination = GetNextDestinationPoint(ref startingIndex);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var localPosition = transform.position;
+        
+        Vector3.MoveTowards(localPosition, _destination, movementSpeed * Time.deltaTime);
+
+        if ((_destination - transform.position).sqrMagnitude < 0.01 * 0.01)
+        {
+            _destination = GetNextDestinationPoint(ref startingIndex);
+        }
+    }
+
+    public Vector3 GetNextDestinationPoint(ref int index)
+    {
+        Vector3 point = path[index].transform.position;
+        index++;
+
+        if (index > path.Length)
+            index = 0;
+
+        return point;
+    }
+}
---
+++ Assets/Scripts/Actor/Obstacle/StaticObstacle.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider), typeof(Rigidbody))]
+public class StaticObstacle : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        
+    }
+}
