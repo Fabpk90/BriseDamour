2738f0d5fd07e0daca51ef8b11ab9f4d21d1885d
Fabpk90 <fabrizio1996@live.fr>
2021-04-17 12:32:41 +0200

    [ADD] Wip on letter handling

---
+++ Assets/Scripts/Letter/LetterManager.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+namespace Letter
+{
+    public class LetterManager : MonoBehaviour
+    {
+        public string pathData;
+
+        private void Start()
+        {
+            //load data
+            //parse data
+            //create the DB of letters
+        }
+    }
+}
