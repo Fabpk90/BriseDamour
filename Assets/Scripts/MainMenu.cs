c50a7c9f1e7f0f104b96ece11b6c8eca5140296b
Enzoe <gozenzoguevara@gmail.com>
2021-04-17 21:49:26 +0200

    Add Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject m_Menu;
+    public GameObject m_MenuCredit;
+
+    //    public GameObject ;
+    //    public GameObject;
+
+    private void Start()
+    {
+        m_Menu.gameObject.SetActive(true);
+        m_MenuCredit.gameObject.SetActive(false);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void GoToCollection()
+    {
+        SceneManager.LoadScene("Collection");
+    }
+
+    public void GoToCredits()
+    {
+        m_Menu.gameObject.SetActive(false);
+        m_MenuCredit.gameObject.SetActive(true);
+    }
+
+    public void QuitCredits()
+    {
+        m_Menu.gameObject.SetActive(true);
+        m_MenuCredit.gameObject.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        //Application.Quit(); //a éviter en cas d'appli web
+    }
+}
