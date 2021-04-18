9680fd2493ad1f74eaa724066c6121389563d7e1
Jerahus <rabierjer@gmail.com>
2021-04-18 12:13:51 +0200

    [ADD] v1 queue cerf volant

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public float speed = 14f;
+    public float speed = 8f;
     public Vector3 forceToApply;
     private Rigidbody rb;
     
@@ -18,20 +18,26 @@ public class PlayerControl : MonoBehaviour
         // Left movement
         if (Input.GetKey(KeyCode.Q))
         {
-            forceToApply.x -= speed * 1.5f;
+            forceToApply.x -= speed * 3f;
         }
 
         // Right movement
         if (Input.GetKey(KeyCode.D))
         {
-            forceToApply.x += speed * 1.5f;
+            forceToApply.x += speed * 3f;
         }
 
 
         // Up movement
         if (Input.GetKey(KeyCode.Z))
         {
-            forceToApply.y += speed * 3f;
+            forceToApply.y += speed * 6f;
+        }
+
+        // Down movement
+        if (Input.GetKey(KeyCode.S))
+        {
+            forceToApply.y -= speed * 3f;
         }
     }
 
